95a884fc3cff664b90cafa8fbf5200a6b62e554f
LucasLouies <71339118+LucasLouies@users.noreply.github.com>
2022-12-07 18:17:02 +0100

    first push Vue.js

---
+++ Pokemon-Project-Frontend/src/components/HomePage.vue
@@ -0,0 +1,20 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="HomePage"> 
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta odit numquam dolores cupiditate consequuntur ratione quas debitis in quibusdam doloribus officiis eius, dolorum corrupti expedita modi rem molestias enim dignissimos.
+  </div>
+</template>
+
+<style scoped>
+.HomePage {
+  margin-bottom: 100px;
+}
+</style>
---
+++ Pokemon-Project-Frontend/src/views/AboutView.vue
@@ -0,0 +1,16 @@
+<script setup>
+import NavBar from '../components/NavBar.vue'
+import Footer from '../components/Footer.vue'
+</script>
+
+<template>
+  <NavBar />
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+  <Footer />
+</template>
+
+<style>
+
+</style>
