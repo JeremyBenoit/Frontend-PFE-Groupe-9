b2b39cfaf91fa72276ff016e6fb1fa516437a214
Elric <elric.deroeux@student.vinci.be>
2022-12-15 17:44:12 +0100

    changes

--- src/components/TeamBuilder.vue
+++ src/components/TeamBuilder.vue
@@ -1,157 +1,176 @@
-<script setup>
-import pokeball from '../assets/pokeball.png'
-import { getAllPokemon, getDefensiveCoverage, getPokemonsFromGen, getPokemonsFromType, getAllType } from '../utils/pokebuildApi'
+<script>
+import pokeball from '../assets/pokeball.png';
+import { getAllPokemon, getDefensiveCoverage, getPokemonsFromGen, getPokemonsFromType, getAllType } from '../utils/pokebuildApi';
+import { createTeam } from '../utils/backendRequests';
 
-let allPoke;
-let allType;
-allPoke = await getAllPokemon();
-allType = await getAllType();
+export default {
+  components: {
+  },
+  data: () => ({
+    loading: [true,"Initialisation..."],
+    generations: [1,2,3,4,5,6,7,8],
+    types: [],
+    allPokemons: [],
+    filterAxises: ["gen", "types"],
+    pokemonTeamPointer: -1,
+    pokemonTeam: [ -1,-1,-1,-1,-1,-1 ],
+    pokeball: pokeball,
+  }),
+  async created() {
+    this.loading[1] = "Chargement des types en cours...";
+    this.types = await getAllType();
+    this.loading[1] = "Chargement des Pokemons en cours...";
+    this.allPokemons = await getAllPokemon();
+    this.loading[0] = false;
+  },
+  methods: {
+    addPokemon(idPokemon) {
+        if(this.pokemonTeam[this.pokemonTeam.length-1]!=-1){
+            return;
+        }
+        this.pokemonTeamPointer++;
+        this.pokemonTeam[this.pokemonTeamPointer] = idPokemon;
+    },
+    removePokemon(indexSlot) {    
+        for (let index = indexSlot; index < this.pokemonTeamPointer; index++) {
+            this.pokemonTeam[index] = this.pokemonTeam[index+1];
+        }
+        this.pokemonTeam[this.pokemonTeamPointer] = -1;
+        this.pokemonTeamPointer--;
+    },
+    async displayDefensiveCoverage() {
+        let defensifeCoverage = await getDefensiveCoverage(this.pokemonTeam);
+        let defensifeCoverageHtml = document.getElementById("defensiveCoverage");
 
-let numberGeneration = 8;
-let filterAxises = ["gen", "types"];
-
-let lastPokePointer = -1;
-let teamPoke = [ -1,-1,-1,-1,-1,-1 ];
-const addPokemon = (idPokemon) => {
-    if(lastPokePointer == 5){
-        console.log("team complete");
-        return;
-    }
-    lastPokePointer++;
-    teamPoke[lastPokePointer] = idPokemon;
-    let htmlPokemon = document.getElementById("Poke"+lastPokePointer);
-    
-    let imagePokemon = document.getElementById(idPokemon);
-    htmlPokemon.innerHTML = imagePokemon.innerHTML;
-}
-
-const removePokemon = (indexPoke) => {
-    let indexPokemonToRemove = parseInt(indexPoke[4])
-    if(lastPokePointer==-1) {
-        return;
-    }
-    
-    for (let index = indexPokemonToRemove; index < lastPokePointer; index++) {
-        teamPoke[index] = teamPoke[index+1];
-
-        let pokemonTempHtml = document.getElementById('Poke'+index);
-        let pokemonNextHtml = document.getElementById('Poke'+(index+1));
-        pokemonTempHtml.innerHTML = pokemonNextHtml.innerHTML;
-    }
-
-    teamPoke[lastPokePointer] = -1;
-    let lastPokemonHtml = document.getElementById('Poke'+lastPokePointer);
-    lastPokemonHtml.innerHTML = `<img src="${pokeball}" class="pokeballPic">`
-
-    lastPokePointer--;
-}
-
-const displayDefensiveCoverage = async () => {
-    let defensifeCoverage = await getDefensiveCoverage(teamPoke);
-    let defensifeCoverageHtml = document.getElementById("defensiveCoverage");
-
-    let tempHtml = ``;
-    defensifeCoverage.forEach(defenseType => {
-        tempHtml += `<div class="${defenseType.result}">${defenseType.name} : ${defenseType.message}</div>`
-    });
-
-    defensifeCoverageHtml.innerHTML = tempHtml;
-}
-
-const filterPokemon = async (filterAxis, filter) => {
-    let allPokemonHtml = document.getElementById("allPokemon");
-    allPokemonHtml.hidden = true;
-
-    let filteredPokemons;
-
-    if (filterAxis == "gen"){
-        filteredPokemons = await getPokemonsFromGen(filter);
-    } else {
-        filteredPokemons = await getPokemonsFromType(filter);
-    }
-    
-    let tempHtml= `<div class="row">`
-    filteredPokemons.forEach(pokemon => {
-        tempHtml += `<div class="col"><img id="filteredPoke${pokemon.id}" src=${pokemon.sprite}> </div>`
-    });
-    tempHtml += `</div>`
-    
-    let filteredPokemonHtml = document.getElementById("filteredPokemon");
-    filteredPokemonHtml.innerHTML = tempHtml;
-    
-    filteredPokemons.forEach(pokemon => {
-        let tempFilteredPokemon = document.getElementById("filteredPoke"+pokemon.id);
-        tempFilteredPokemon.addEventListener("click", () => {
-            addPokemon(pokemon.id);
+        let tempHtml = ``;
+        defensifeCoverage.forEach(defenseType => {
+            tempHtml += `<div class="${defenseType.result}">${defenseType.name} : ${defenseType.message}</div>`
         });
-    })
-}
 
-const resetFilters = () => {
-    let filteredPokemonHtml = document.getElementById("filteredPokemon");
-    filteredPokemonHtml.innerHTML = ``;
+        defensifeCoverageHtml.innerHTML = tempHtml;
+    },
+    async filterPokemon(filterAxis, filter){
+        let allPokemonHtml = document.getElementById("allPokemon");
+        allPokemonHtml.hidden = true;
 
-    let allPokemonHtml = document.getElementById("allPokemon");
-    allPokemonHtml.hidden = false;
+        let filteredPokemons;
+
+        if (filterAxis == "gen"){
+            filteredPokemons = await getPokemonsFromGen(filter);
+        } else {
+            filteredPokemons = await getPokemonsFromType(filter);
+        }
+    
+        let tempHtml= `<div class="row">`
+        filteredPokemons.forEach(pokemon => {
+            tempHtml += `<div class="col"><img id="filteredPoke${pokemon.id}" src=${pokemon.sprite}> </div>`
+        });
+        tempHtml += `</div>`
+    
+        let filteredPokemonHtml = document.getElementById("filteredPokemon");
+        filteredPokemonHtml.innerHTML = tempHtml;
+    
+        filteredPokemons.forEach(pokemon => {
+            let tempFilteredPokemon = document.getElementById("filteredPoke"+pokemon.id);
+            tempFilteredPokemon.addEventListener("click", () => {
+                this.addPokemon(pokemon.id);    
+            });
+        })
+    },
+    resetFilters() {
+        let filteredPokemonHtml = document.getElementById("filteredPokemon");
+        filteredPokemonHtml.innerHTML = ``;
+
+        let allPokemonHtml = document.getElementById("allPokemon");
+        allPokemonHtml.hidden = false;
+    },
+    async createTeamHandler(e) {
+        const form = e.target;
+        if(this.pokemonTeam[this.pokemonTeam.length-1]!=-1){
+            this.loading[0] = true;
+            this.loading[1] = "Equipe en cours de création";
+            const res = await createTeam(form.teamName.value, localStorage.pseudo, this.pokemonTeam, localStorage.token);
+            this.loading[0] = false;
+        }
+    }
+  }
 }
 </script>
 
 <template>
-    <div class="row pokemonTeam">
-        <div v-for="(poke, index) in teamPoke" class="col">
-            <div v-if="poke == -1">
-                <div v-bind:id="'Poke'+index" v-on:click="removePokemon('Poke'+index)"> <img :src="pokeball" class="pokeballPic"> </div>
-            </div>
-        </div>
-    </div>
-    <div class="container filterButtons">
-        <div class="row">
-            <div class="col">  
-                <div class="dropdown">
-                    <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                        Types
-                    </button>
-                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                        <li v-for="types in allType">
-                            <a class="dropdown-item" v-on:click="filterPokemon(filterAxises[1], types.name)"> {{ types.name }} </a>
-                        </li>
-                    </ul>
+    <div v-if="loading[0]">
+        <img class="loadingGif" src="../assets/images/loading.gif">
+        <h3 class="loadingMessage">{{loading[1]}}</h3>
+    </div>  
+    <div v-else>
+        <div class="row pokemonTeam">
+            <div v-for="index in pokemonTeam.length" class="col">
+                <div v-if="pokemonTeam[index-1] == -1">
+                    <div v-bind:id="'PokemonSlot'+index">
+                        <img :src="pokeball" class="teamPic">
+                    </div>
+                </div>
+                <div v-else>
+                    <div>
+                        <img :src="allPokemons[pokemonTeam[index-1]-1].sprite" @click="removePokemon(index)" class="teamPic">
+                    </div>
                 </div>
             </div>
-            <div class="col">  
-                <div class="dropdown">
-                    <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                        Generation
-                    </button>
-                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                        <li v-for="n in numberGeneration">
-                            <a class="dropdown-item" v-on:click="filterPokemon(filterAxises[0], n)">gen {{ n }}</a>
-                        </li>
-                    </ul>
+        </div>
+        <div class="container filterButtons">
+            <div class="row">
+                <div class="col">
+                    <button class="btn btn-primary" type="button" v-on:click="resetFilters()">Voir tout</button>
+                </div>
+                <div class="col">  
+                    <div class="dropdown">
+                        <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                            Types
+                        </button>
+                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                            <li v-for="t in types">
+                                <a class="dropdown-item" v-on:click="filterPokemon(filterAxises[1], t.name)"> {{ t.name }} <img class="typeImage" :src="t.image" ></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col">  
+                    <div class="dropdown">
+                        <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                            Generation
+                        </button>
+                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                            <li v-for="n in generations">
+                                <a class="dropdown-item" v-on:click="filterPokemon(filterAxises[0], n)">gen {{ n }}</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="strengthWeakness col">
+                    <button class="btn btn-primary" v-on:click="displayDefensiveCoverage()"> Vérifier forces et faiblesses </button>
+                </div>
+                <div class="col row" v-if="pokemonTeam[pokemonTeam.length-1]!=-1">
+                    <form id="createTeamForm" v-on:submit.prevent="createTeamHandler">
+                        <input type="text" placeholder="Nom d'équipe" name="teamName" required class="col">
+                        <button id="createTeamButton" type="submit" class="btn btn-primary col">Créer l'équipe</button>
+                    </form>
+                </div>
+                <div id="defensiveCoverage"></div>
+            </div>
+        </div>
+        <div id="allPokemon">
+            <div class="row">
+                <div v-for="poke in this.allPokemons" class="col" v-bind:id="poke.id">
+                    <img :src="poke.sprite" @click="addPokemon(poke.id)">
                 </div>
             </div>
-            <div class="col-5"></div>
-            <div class="col-3">
-                <button class="btn btn-primary" type="button" v-on:click="resetFilters()">Voir tous les pokemons</button>
-            </div>
         </div>
-        <div class="strengthWeakness">
-            <button class="btn btn-primary" v-on:click="displayDefensiveCoverage()"> Vérifier forces et faiblesses </button>
-            <div id="defensiveCoverage"></div>
-        </div>
+        <div id="filteredPokemon"></div>
     </div>
-    <div id="allPokemon">
-        <div class="row">
-            <div v-for="poke in allPoke" class="col" v-bind:id="poke.id">
-                <img :src="poke.sprite" v-on:click="addPokemon(poke.id)">
-            </div>
-        </div>
-    </div>
-    <div id="filteredPokemon"></div>
 </template>
 
 <style>
-.pokeballPic {
+.teamPic {
     width: 50%;
     height: auto;
 }
@@ -161,13 +180,21 @@ const resetFilters = () => {
     margin-bottom: 10px;
 }
 
-.strengthWeakness{
+.filterButtons{
     margin-top: 5px;
     margin-bottom: 5px;
 }
 
-.filterButtons{
-    margin-top: 5px;
-    margin-bottom: 5px;
+.loadingGif, .loadingMessage{
+    width: 20%;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    font-style: oblique;
+    text-align: center;
+}
+
+.typeImage{
+    width: 15%;
 }
 </style>
--- src/views/TeamPokemonView.vue
+++ src/views/TeamPokemonView.vue
@@ -3,29 +3,9 @@ import NavBar from '../components/NavBar.vue'
 import Footer from '../components/Footer.vue'
 import TeamPokemon from '../components/TeamPokemon.vue'
 </script>
+
 <template>
   <NavBar />
-  <Suspense>
-    <template #default>
-      <TeamPokemon :teamId="$route.params.id"/>
-    </template>
-    <template #fallback>
-      <div role="status">
-        <img class="loadingGif center" src="../assets/images/loading.gif">
-      </div>
-    </template>
-  </Suspense>
-
+  <TeamPokemon :teamId="$route.params.id"/>
   <Footer />
 </template>
-
-<style scoped>
-.center{
-  display: block;
-  margin-left: auto;
-  margin-right: auto;
-}
-.loadingGif{
-  width: 20%;
-}
-</style>
